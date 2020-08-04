cea1e4ad145d7aa92508e1c1a17718c91be56e99
Miloš Havlíček <miloshavlicek@gmail.com>
2020-08-04 13:29:47 +0200

    + 404 page

--- scully.sales-website.config.ts
+++ scully.sales-website.config.ts
@@ -1,6 +1,9 @@
+import { getHttp404Plugin } from '@gammastream/scully-plugin-http404';
 import { getSitemapPlugin } from '@gammastream/scully-plugin-sitemap';
 import { registerNgxTranslateRouter } from '@gilsdav/ngx-translate-router-scully-plugin';
 import { ScullyConfig, setPluginConfig } from '@scullyio/scully';
+
+const Http404Plugin = getHttp404Plugin();
 
 const SitemapPlugin = getSitemapPlugin();
 setPluginConfig(SitemapPlugin, {
@@ -21,6 +24,7 @@ export const config: ScullyConfig = {
   projectRoot: './src',
   projectName: 'sales-website',
   outDir: './dist/static',
+  defaultPostRenderers: [Http404Plugin],
   routes: {
     '/blog/:slug': {
       type: 'contentFolder',
